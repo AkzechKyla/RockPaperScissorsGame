947a10b2cf83fc20bc4eb06c8cfdf12955446f94
Kyla <kflmorcillos@gmail.com>
2024-08-19 19:53:59 +0800

    Add scoring and display feedback

--- scripts/main.js
+++ scripts/main.js
@@ -2,46 +2,70 @@
 1. Buttons
 2. Get computer's move
 3. Compare to player's move
+4. Score
 */
 
-const availableMoves = ['rock', 'paper', 'scissors'];
+const availableMoves = ['Rock', 'Paper', 'Scissors'];
+const score = {
+    won: 0,
+    lost: 0,
+    draw: 0
+}
 
 function playGame(playerMove) {
     let randomMove = Math.floor(Math.random() * availableMoves.length);
     let computerMove = availableMoves[randomMove];
+    let gameState = '';
 
     if (playerMove === availableMoves[0]) {
         // if player's move is rock
         if (computerMove === availableMoves[1]) {
             // if computer's move is paper
-            console.log('Paper beats Rock. You lost.');
+            gameState = 'lost';
         } else if (computerMove === availableMoves[2]) {
             // if computer's move is scissors
-            console.log('Rock beats Scissors. You win.')
+            gameState = 'won';
         } else if (playerMove === availableMoves[0]) {
-            console.log("It's a TIE!");
+            gameState = 'draw';
         }
     } else if (playerMove === availableMoves[1]) {
         // if player's move is paper
         if (computerMove === availableMoves[0]) {
             // if computer's move is rock
-            console.log('Paper beats Rock. You win.');
+            gameState = 'won';
         } else if (computerMove === availableMoves[2]) {
             // if computer's move is scissors
-            console.log('Scissors beat Paper. You lost.');
+            gameState = 'lost';
         } else if (playerMove === availableMoves[1]) {
-            console.log("It's a TIE!");
+            gameState = 'draw';
         }
     } else if (playerMove === availableMoves[2]) {
         // if player's move is scissors
         if (computerMove === availableMoves[0]) {
             // if computer's move is rock
-            console.log('Rock beats Scissors. You lost.');
+            gameState = 'won';
         } else if (computerMove === availableMoves[1]) {
             // if computer's move is paper
-            console.log('Scissors beat Paper. You win.');
+            gameState = 'lost';
         } else if (playerMove === availableMoves[2]) {
-            console.log("It's a TIE!");
+            gameState = 'draw';
         }
+    }
+
+    displayFeedbackAndScore(gameState, playerMove, computerMove);
+
+    console.log(score);
+}
+
+function displayFeedbackAndScore(gameState, playerMove, computerMove) {
+    if (gameState === 'won') {
+        score.won += 1;
+        console.log(`${playerMove} beats ${computerMove}. You won.`);
+    } else if (gameState === 'lost') {
+        score.lost += 1;
+        console.log(`${computerMove} beats ${playerMove}. You lost.`);
+    } else if (gameState === 'draw') {
+        score.draw += 1;
+        console.log("It's a TIE!");
     }
 }
